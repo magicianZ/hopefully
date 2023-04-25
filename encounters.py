26bee4b55a1b40dcd4dd5e3448f6ab952292f45e
ChubyBubles <Raym.chen1217@gmail.com>
2023-04-25 10:57:58 -0400

    first encounter

---
+++ encounters.py
@@ -0,0 +1,7 @@
+print("You're exploring a forest near the village you started at. While exploring you encounter a monster, you can choose fight it or run away...")
+user_input = input("Do you choose to fight it or run way? (Fight/Run)").capitalize()
+if user_input == "Fight":
+    print("You chose to fight the monster.")
+elif user_input == "Run":
+    print("Nah you ain't running go fight that thing.")
+
