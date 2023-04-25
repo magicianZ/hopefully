da6660bd99e7958cc6f48cf6d694d9a545d84faa
Ethan De Jesus <ethanawesome702@gmail.com>
2023-04-25 15:38:43 -0400

    YESSIR IM DONE

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ import random
 
 class Player:
    
-    def __init__(self,HP,Weapon,Name,Damage,energy_point,magic_point):
+    def __init__(self,HP,Weapon,Name,Damage,energy_point,magic_point,magic_damage):
         self.HP = HP
         self.Weapon = Weapon
         self.Name = Name
         self.Damage = Damage
         self.magic_point = magic_point
         self.energy_point = energy_point
-        
+        self.magic_damage = magic_damage
 
 
     def attack(self,target):
@@ -21,8 +21,6 @@ class Player:
             self.energy_point = self.energy_point - 1
             print(f'{target.Name} is {target.HP} HP and you have {self.energy_point}energy points')
             print('-----------------------------')
-           
-
         else:
             print('You got no energy points, you need to heal homie')
 
@@ -30,64 +28,65 @@ class Player:
     def heal(self,target):
         real_HP = self.HP + 25
         self.HP = real_HP
-        print(f'{self.Name} HP is now {real_HP}')
-        print('----------------------------')
         self.energy_point = self.energy_point + 1
-    def magic_attack(self):
-        self.Damage = self.Damage + 50
-        self.magic_points - 1
+        print(f'{self.Name} HP is now {real_HP} and you have {self.energy_point}energy points')
+        print('----------------------------')
+    
         
-    def revert_Damage(self):
-        self.Damage = self.Damage
+    def magic_dmg(self,target):
+        if self.magic_point > 0:
+            damage_taken = target.HP - self.magic_damage
+            target.HP = damage_taken
+            self.magic_point = self.magic_point - 1
+            print(f'{target.Name} is {target.HP} HP and you have {self.magic_point}magic points')
+            print('-----------------------------')
+        else:
+            print('You got no magic points homie')
+            
     def turn(self,target):
-        user_input = input(f'{self.Name}s turn - Would you like to, Attack , Heal,or use a magic attack').capitalize()
+        user_input = input(f'{self.Name}s turn - Would you like to - Attack , Heal or Magic').capitalize()
         if user_input == 'Attack':
             self.attack(target)
         elif user_input == 'Heal':
-            NPC.heal(Ly)
-        elif user_input == 'Magic Attack':
-            print('magic')
+            self.heal(target)
+        elif user_input == 'Magic':
+            self.magic_dmg(target)
+
+
+
     def AI(self,target):
         self.list = ['Attack','Attack','Attack','Heal']
         get_lucky = random.choice(self.list)
-        if get_lucky == 'Attack':
-            self.attack(target)
-            print('The NPC attacked you...')
-        elif get_lucky == 'Heal':
-            self.heal(Ly)
-            print('The NPC Healed...')
-        else:
-            print('Ai sucks')
+        if self.energy_point > 0:
+             if get_lucky == 'Attack':
+                self.attack(target)
+                print('The NPC attacked you...')
+        elif self.energy_point == 0:
+            if get_lucky == 'Attack':
+                get_lucky == 'Heal'
+            if get_lucky == 'Heal':
+                self.heal(Ly)
+                print('The NPC Healed...')
+            else:
+                print('Ai sucks')
 
 
 
 
       
-            
-    
         
-    
-    
-    
+
+Ly = Player(100,"Sword","Lie Lee",50,3,1,100)
+NPC = Player(500,"Sword","NPC",10,3,1,20)
 
 
-
-
-
-
-Ly = Player(100,"Sword","Lie Lee",50,3,1)
-
-NPC = Player(500,"Sword","NPC",10,3,1)
 LysTurn = True
 EnemiestURN = False
-""" Ly.turn(NPC)
-print(NPC.HP)
-
-NPC.turn(Ly)
-print(Ly.HP) """
-
-
 running = True
+
+
+
+
 while running:
     if LysTurn:
         Ly.turn(NPC)
