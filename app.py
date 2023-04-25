244f81dc7f224ef55a68f897a04f207e8c293f57
Ethan De Jesus <ethanawesome702@gmail.com>
2023-04-25 12:23:43 -0400

    yessir

--- app.py
+++ app.py
@@ -15,16 +15,19 @@ class Player:
 
 
     def attack(self,target):
-        damage_taken = target.HP - self.Damage
-        target.HP = damage_taken
+        if self.energy_point > 0:
+            damage_taken = target.HP - self.Damage
+            target.HP = damage_taken
+            self.energy_point = self.energy_point - 1
+            print(self.energy_point)
+        else:
+            print('You got no energy points, you need to heal homie')
 
       
     def heal(self,target):
         real_HP = self.HP + 25
         self.HP = real_HP
         print(f'Your HP is now {real_HP}')
-        
-    
         self.energy_point + 1
     def magic_attack(self):
         self.Damage = self.Damage + 50
@@ -36,14 +39,8 @@ class Player:
         user_input = input(f'{self.Name}s turn - Would you like to, Attack , Heal,or use a magic attack').capitalize()
         if user_input == 'Attack':
             self.attack(target)
-            #something.damage()
-            print('attack')
-            
         elif user_input == 'Heal':
             NPC.heal(Ly)
-            print('block')
-            #something.block()
-            
         elif user_input == 'Magic Attack':
             print('magic')
             #something.magic_attack()
@@ -61,7 +58,7 @@ class Player:
 
 Ly = Player(100,"Sword","Lie Lee",50,3,1)
 
-NPC = Player(200,"Sword","NPC",10,3,1)
+NPC = Player(500,"Sword","NPC",10,3,1)
 LysTurn = True
 EnemiestURN = False
 """ Ly.turn(NPC)
