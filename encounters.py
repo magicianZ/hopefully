1affcf78303ff1b8ef73aff7d00cdee04c973de6
ChubyBubles <Raym.chen1217@gmail.com>
2023-04-26 10:54:28 -0400

    hhh

--- encounters.py
+++ encounters.py
@@ -114,13 +114,17 @@ def encounter():
             break
 
 print("You're exploring a forest near the village you started at. While exploring you encounter a monster, you can choose fight it or run away...")
-user_input = input("Do you choose to fight it or run way? (Fight/Run)").capitalize()
-if user_input == "Fight":
-    print("You chose to fight the monster.")
-    encounter()
-if user_input == "Run":
-    print("Nah you ain't running, go fight that thing.")
-    encounter()
+for i in range(3):
+    user_input = input("Do you choose to fight it or run way? (Fight/Run)").capitalize()
+
+    if user_input == "Fight":
+        print("You chose to fight the monster.")
+        encounter()
+    elif user_input == "Run":
+        print("Nah you ain't running, go fight that thing.")
+        encounter()
+    else:
+        print("Homie that ain't a valid response.")
 
 
 
